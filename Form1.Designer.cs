d951390fd2ef8294dfd858df9c35bd09496b8843
Scorpionguy <lapinfedor11@gmail.com>
2023-04-27 20:19:33 +0300

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace hak
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            textBox1 = new TextBox();
+            comboBox1 = new ComboBox();
+            menuStrip1 = new MenuStrip();
+            менюToolStripMenuItem = new ToolStripMenuItem();
+            открытьФайлToolStripMenuItem = new ToolStripMenuItem();
+            menuStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(32, 46);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 29);
+            button1.TabIndex = 0;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(201, 343);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(595, 60);
+            textBox1.TabIndex = 2;
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(32, 81);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(151, 28);
+            comboBox1.TabIndex = 3;
+            comboBox1.SelectedIndexChanged += comboBox1_SelectedIndexChanged_1;
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.ImageScalingSize = new Size(20, 20);
+            menuStrip1.Items.AddRange(new ToolStripItem[] { менюToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(800, 28);
+            menuStrip1.TabIndex = 4;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // менюToolStripMenuItem
+            // 
+            менюToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { открытьФайлToolStripMenuItem });
+            менюToolStripMenuItem.Name = "менюToolStripMenuItem";
+            менюToolStripMenuItem.Size = new Size(65, 24);
+            менюToolStripMenuItem.Text = "Меню";
+            // 
+            // открытьФайлToolStripMenuItem
+            // 
+            открытьФайлToolStripMenuItem.Name = "открытьФайлToolStripMenuItem";
+            открытьФайлToolStripMenuItem.Size = new Size(224, 26);
+            открытьФайлToolStripMenuItem.Text = "Открыть файл...";
+            открытьФайлToolStripMenuItem.Click += открытьФайлToolStripMenuItem_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(comboBox1);
+            Controls.Add(textBox1);
+            Controls.Add(button1);
+            Controls.Add(menuStrip1);
+            MainMenuStrip = menuStrip1;
+            Name = "Form1";
+            Text = "Form1";
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private TextBox textBox1;
+        private ComboBox comboBox1;
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem менюToolStripMenuItem;
+        private ToolStripMenuItem открытьФайлToolStripMenuItem;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,76 @@
+using System.Text.RegularExpressions;
+using System.IO;
+using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Wordprocessing;
+
+namespace hak
+{
+    public partial class Form1 : Form
+    {
+        string file = "Doc.txt";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //string serverName = "localhost";
+            //string userName = "root";
+            //string dbName = "hakaton";
+            //string port = "3306";
+            //string password = "1234";
+            //string connectionStr = "server" + serverName + ";user=" + userName + ";database" + dbName + ";port" + port + ";password=" + password; 
+
+
+            
+            string firstPattern = @"~!(.*?)!~"; // –егул€рное выражение дл€ поиска текста между "!"
+
+
+            using (StreamReader sr = new StreamReader(file))
+            {
+                //штука котора€ будет копировать выведенный текст в буфер обмена
+                string line;
+                string[] box;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    MatchCollection matches = Regex.Matches(line, firstPattern);
+                    if (matches.Count != 0)
+                    {
+                        comboBox1.Items.Add(matches[0]);
+                    }
+                    foreach (Match match in matches)
+                    {
+                        textBox1.Text += match.Groups[1].Value; // ¬ывод содержимого между "!" на консоль
+                        textBox1.Text += Environment.NewLine;
+                    }
+                }
+            }
+
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void открыть‘айлToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "txt (.txt)|*.txt; *.docx; *.doc;";
+            ofd.InitialDirectory = "C:\\";
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                file = ofd.FileName;
+            }
+
+        }
+    }
+}
